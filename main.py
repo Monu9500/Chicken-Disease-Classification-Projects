fc04bca92cbf2d7ed6b0c300e6544ceb6e7d3a3a
Pritam Ghosh <pritamghoshdhamsa@gmail.com>
2026-01-12 19:09:01 +0530

    training added

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import pathlib
+pathlib.WindowsPath.endswith = lambda self, suffix: str(self).endswith(suffix)
+pathlib.WindowsPath.format = lambda self, *args, **kwargs: str(self).format(*args, **kwargs)
+
+
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+#from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -30,18 +36,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Training"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-
-
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
