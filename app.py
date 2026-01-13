e4bf6ef292d0861f8c70a31f74b460dcbf020daa
Pritam Ghosh <pritamghoshdhamsa@gmail.com>
2026-01-13 13:54:45 +0530

    App Update

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def predictRoute():
 clApp = ClientApp()  
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8082)
 
 # if __name__ == "__main__":
 #     clApp = ClientApp()
